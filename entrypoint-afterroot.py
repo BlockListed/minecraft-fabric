4f05033dcdb1aa2dc95d807e0c6de953c1f4ec6a
BlockListed <44610569+BlockListed@users.noreply.github.com>
2023-04-22 17:03:49 +0200

    make python script args more modular

--- entrypoint-afterroot.py
+++ entrypoint-afterroot.py
@@ -74,31 +74,11 @@ mod_download.wait()
 
 ram = os.environ["RAM"]
 
+ram_args = [f"-Xms{ram}", f"-Xmx{ram}"]
+
+gc_args = "-XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true".split()
+
+args = ram_args + gc_args + ["-jar", "fabric.jar", "--nogui"]
+
 os.chdir("/minecraft")
-os.execvp("java", [
-    f"-Xms{ram}",
-    f"-Xmx{ram}",
-    "-XX:+UseG1GC",
-    "-XX:+ParallelRefProcEnabled",
-    "-XX:MaxGCPauseMillis=200",
-    "-XX:+UnlockExperimentalVMOptions",
-    "-XX:+DisableExplicitGC",
-    "-XX:+AlwaysPreTouch",
-    "-XX:G1NewSizePercent=30",
-    "-XX:G1MaxNewSizePercent=40",
-    "-XX:G1HeapRegionSize=8M",
-    "-XX:G1ReservePercent=20",
-    "-XX:G1HeapWastePercent=5",
-    "-XX:G1MixedGCCountTarget=4",
-    "-XX:InitiatingHeapOccupancyPercent=15",
-    "-XX:G1MixedGCLiveThresholdPercent=90",
-    "-XX:G1RSetUpdatingPauseTimePercent=5",
-    "-XX:SurvivorRatio=32",
-    "-XX:+PerfDisableSharedMem",
-    "-XX:MaxTenuringThreshold=1",
-    "-Dusing.aikars.flags=https://mcflags.emc.gs",
-    "-Daikars.new.flags=true",
-    "-jar",
-    "/minecraft/fabric.jar",
-    "--nogui",
-])
+os.execvp("java", args)
