d121149cbdfd466bcfd6164fdf4462007c36e117
BlockListed <44610569+BlockListed@users.noreply.github.com>
2023-06-13 17:11:38 +0200

    change tag.sh to allow multiline tag messages

--- tag.sh
+++ tag.sh
@@ -8,4 +8,4 @@ GAME_VERSION="1.20"
 [ $# != 1 ] && (echo "SINGLE ARGUMENT REQUIRED" && exit 1)
 [ -z "$1" ] && (echo "MESSAGE EMPTY" && exit 1)
 
-git tag -s "${GAME_VERSION}_${date}" -m "$1"
+echo git tag -s "${GAME_VERSION}_${date}" -m "$1"
